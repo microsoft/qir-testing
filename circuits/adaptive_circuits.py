88ab9ed0d4f4c557062fa5718d14686b7c8f012e
Ian Davis <ian.f.davis@microsoft.com>
2022-07-12 12:58:38 -0700

    Adding copyright headers

--- circuits/adaptive_circuits.py
+++ circuits/adaptive_circuits.py
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
 from qiskit import ClassicalRegister, QuantumCircuit
 from qiskit.circuit import Clbit
 
--- circuits/gates.py
+++ circuits/gates.py
@@ -1,3 +1,5 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
 
 from typing import Any, Generator, List
 
